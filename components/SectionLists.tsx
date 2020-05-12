7a9164bba7fdd8686000f9e798c38723e72564c7
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-05-12 20:01:04 +0900

    [add] SelectionLists

---
+++ components/SectionLists.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {SectionList, StyleSheet, Text, View} from 'react-native';
+
+const SectionLists: React.FC = () => {
+  const datas = [
+    {title: 'D', data: ['Devian', 'Dan', 'Dominic']},
+    {title: 'J', data: ['Jackson', 'James', 'Jillian']},
+  ];
+
+  return (
+    <View>
+      <SectionList sections={datas} />
+    </View>
+  );
+};
