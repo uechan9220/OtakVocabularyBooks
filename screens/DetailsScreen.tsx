26e182e022748f89d808fccc4965d9776ef87a0d
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-05-16 23:22:26 +0900

    [change] style change

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,14 +1,43 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 const DetailsScreen: React.FC = ({route}: any) => {
   const {item} = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{item.name}</Text>
-      <Text>{item.details}</Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{item.name}</Text>
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.detail}>{item.details}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  titleContainer: {
+    paddingTop: 60,
+    paddingBottom: 20,
+    borderBottomWidth: 2,
+  },
+  title: {
+    fontSize: 40,
+  },
+  detailsContainer: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200
+  },
+  detail: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
 
 export default DetailsScreen;
